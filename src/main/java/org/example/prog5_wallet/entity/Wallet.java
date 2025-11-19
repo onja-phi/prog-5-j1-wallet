ad4c6ca3d2ea14d38d0ae8bc9748618f9494d084
onja-phi <153230779+onja-phi@users.noreply.github.com>
2025-11-19 19:34:54 +0300

    fix: adjust porteCards type and add addCard method
    Removed portCard variable and added porteCards list to manage cards.

--- src/main/java/org/example/prog5_wallet/entity/Wallet.java
+++ src/main/java/org/example/prog5_wallet/entity/Wallet.java
@@ -1,11 +1,15 @@
 package org.example.prog5_wallet.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wallet {
     private String color;
-    private boolean portCard;
     private double mass;
     private String brand;
     private double account;
+
+    private List<String> porteCards = new ArrayList<>();
 
     public void getMoney(double montant) {
         account -= montant;
@@ -16,7 +20,11 @@ public class Wallet {
     public double checkMoney() {
         return account;
     }
-    //public void addCard();
+
+    public void addCard(String card) {
+        porteCards.add(card);
+    }
+        
     public void lostMoney(double montant){
         account -= montant;
     }
